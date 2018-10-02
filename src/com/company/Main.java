ef49117e6a183b05fddc86bbf4bf6e75c21e7011
EvgenyRosovsky <rosovsky@protonmail.com>
2018-10-02 18:20:08 +0300

    sorting numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+// sorting from small to bigger
         int arr[] = new int[5];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 10);
