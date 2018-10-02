68ea550b94651da3304fae4d7c0361dc188dd3f0
EvgenyRosovsky <rosovsky@protonmail.com>
2018-10-02 18:10:44 +0300

    first commit | test

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.lang.String;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int arr[] = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10);
+            System.out.print(arr[i] + "  ");
+        }
+        System.out.print("\nSorted: \n");
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "  ");
+        }
+    }
+
+}
